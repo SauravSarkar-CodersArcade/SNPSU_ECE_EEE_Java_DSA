88433553a359adc4a02d2687c2b9a282e2a335b1
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2026-02-23 16:45:58 +0530

    Added all latest codes & PDF notes till D1-S3

---
+++ SNPSU_B1_ECE_EEE/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 20;
+        char c = '*';
+        double d = 6.5D;
+        float f = 6.5F;
+        byte b = 127;
+        int $name = 2;
+        String r1 = (2 > 3) ? "true" : "false";
+        boolean r2 = (2 > 3) ? true : false;
+        int r3 = (2 > 3) ? 1 : 0;
+        System.out.println(r1 + " " + r2 + " " + r3);
+        System.out.println((2 > 3) ? 'T' : 'F');
+    }
+}
